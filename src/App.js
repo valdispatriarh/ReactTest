a8a75639f375d7216271b02c909e1404b4624fae
Vlad <valdispatriarh@gmail.com>
2024-08-22 22:28:23 +0200

    next

--- src/App.js
+++ src/App.js
@@ -1,70 +1,36 @@
-import React from 'react';
- 
+
+import React, { useState } from 'react';
+
 function App() {
-let a = [];
-let b = "Vlad"
-let c = "Bond"
-let x = false
-let text
-const isAdult = 1;
-let sms
-let isAdmin = true
-let qwe
-let age = false
-const isAuth = false;
+    let [userDisOrNonDis, setB] = useState(false);
+	let newState = false
+    // let handleSetB = () => {
+    //     setB(prevState => {
 
+    //         document.getElementById("btn1").disabled = !newState;
+    //         document.getElementById("btn2").disabled = !newState;
+    //         document.getElementById("btn3").disabled = newState;
+	// 		newState = !userDisOrNonDis;
+    //         console.log(newState);
+    //         return newState;
+    //     });
+    // };
 
+   
 
-function show(user) {
-  alert(user);
+    return (
+        <div>
+            <button id="btn1" onClick={() => setB(true)} disabled={userDisOrNonDis}>
+                blok
+            </button>
+            <button id="btn2">
+                <img src="foto-bl.png" alt="Button 2" />
+            </button>
+            <button id="btn3" onClick={() => setB(false)} disabled={!userDisOrNonDis}>
+                unblock
+            </button>
+        </div>
+    );
 }
-function getDigitsSum(num) {
-  let sum = 0
-  let strnum = String(num)
-  for(let i = 0; i < strnum.length; i++){
-    sum = sum + +strnum[i]
-  }
-  return sum
-}
-for(let i = 1; i < 11; i++){
-  a.push(<li key={i}>{i}</li>)
-}
-if(x){
-  text = <p>true</p>
-} 
-if(isAdult >= 18){
-  sms = <div>hello</div>
-} else {
-  sms = <div>You is less 18 yeas old</div>
-}
-if(isAdmin){
-  qwe = 
-  <><p>a</p>
-  <p>d</p>
-  <p>m</p>
-  <p>i</p>
-  <p>n</p>
-</>
-}
-  
 
-
-
-
-return <>
-<ul>{a}</ul>
-<div>result:{Math.sqrt(4) }  </div>
-<div>result:{b+" "+c}  </div>
-{text}
-{sms}
-{qwe}
-{age ? <p>hey</p> : <p>wait 18</p>}
-{!isAuth && <div><p>please, sing in</p></div>}
-<p>{getDigitsSum(12345)}</p>
-<button value="1" onClick={()=>show("1")}>1</button>
-<button onClick={()=>show("2")}>2</button>
-<button onClick={()=>show("3")}>3</button>
-<button onClick={console}>console</button>
-</>
-}
-export default App;
+export default App; 
